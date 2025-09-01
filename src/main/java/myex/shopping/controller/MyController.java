0720242236c2da84af90d735d4320c885cd8f712
sunjae1 <mk35plkr@naver.com>
2025-09-01 23:31:58 +0900

    first test 202509012131

--- src/main/java/myex/shopping/controller/MyController.java
+++ src/main/java/myex/shopping/controller/MyController.java
@@ -1,4 +1,16 @@
 package myex.shopping.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MyController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String start() {
+        return " 안녕 하세요";
+
+    }
 }
