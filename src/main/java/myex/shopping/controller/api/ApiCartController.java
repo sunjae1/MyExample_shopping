99e4e848cdbaaa15e45b023501fd16e04600e5a1
sunjae1 <mk35plkr@naver.com>
2025-10-26 22:29:39 +0900

    modi ApiCartController : Session -> DB

--- src/main/java/myex/shopping/controller/api/ApiCartController.java
+++ src/main/java/myex/shopping/controller/api/ApiCartController.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import myex.shopping.domain.Cart;
 import myex.shopping.domain.Item;
+import myex.shopping.domain.User;
 import myex.shopping.dto.CartDto;
 import myex.shopping.dto.RemoveCartDto;
 import myex.shopping.form.CartForm;
+import myex.shopping.repository.CartRepository;
 import myex.shopping.repository.ItemRepository;
 import myex.shopping.repository.memory.MemoryItemRepository;
+import myex.shopping.service.CartService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,29 +26,48 @@ import java.util.Optional;
 @Validated
 public class ApiCartController {
     private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
+    private final CartService cartService;
+
 
 
     //한 상품에 대한 주문 페이지에서 정보가 넘어오면 장바구니에 저장.
     //cartForm 에서 id - item.id 매핑됨.
     // id, price, quantity --> CartForm : id(상품 id 넣기), quantity 매핑.
 
-    //?????? 이상한데 itemId 하거나 CartForm id 쓰거나 한개만 써.  --> itemId로.
+    //itemId나 CartForm id 중 하나만 쓰기. --> itemId로.
     @PostMapping("/{itemId}/cart")
     public ResponseEntity<CartDto> addToCart(@PathVariable @Positive(message = "양수만 입력가능합니다.") Long itemId,
                                           @Valid @RequestBody CartForm cartForm,
                                           HttpSession session) {
 
 
-        Cart cart = getOrCreateCart(session);
+//        Cart cart = getOrCreateCart(session);
 //        Item findItem = itemRepository.findById(cartForm.getId());
         Optional<Item> findItemOpt = itemRepository.findById(itemId);
-
+        
+        
+        
         if (findItemOpt.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         Item findItem = findItemOpt.get();
+        
+        //재고 수량 초과로 장바구니 담을 시
+        if(findItem.getQuantity() < cartForm.getQuantity()) {
+            return ResponseEntity.badRequest().build(); //클라이언트 오류
+        }
+
+        User loginUser = (User) session.getAttribute("loginUser");
+        Cart cart = cartService.findOrCreateCartForUser(loginUser);
+
         //아이템과 수량.
-        cart.addItem(findItem, cartForm.getQuantity());
+        boolean result = cart.addItem(findItem, cartForm.getQuantity());
+
+        //전체 장바구니 수량이 총 수량을 넘었을 때
+        if (result == false) {
+            return ResponseEntity.badRequest().build();
+        }
 
         CartDto cartDto = new CartDto(cart);
         return ResponseEntity.ok(cartDto);
@@ -55,9 +77,15 @@ public class ApiCartController {
     //장바구니 전체 보여주는 뷰. cartItem List를 보내줌.
     @GetMapping("/cartAll")
     public ResponseEntity<CartDto> cartAll(HttpSession session) {
-        Cart cart = getOrCreateCart(session);
-        CartDto cartDto = new CartDto(cart);
-        return ResponseEntity.ok(cartDto);
+//        Cart cart = getOrCreateCart(session);
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        return cartRepository.findByUser(loginUser)
+                .map(cart -> ResponseEntity.ok(new CartDto(cart)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+//        CartDto cartDto = new CartDto(cart);
+//        return ResponseEntity.ok(cartDto);
 
     }
 
@@ -73,10 +101,19 @@ public class ApiCartController {
         }
         Item findItem = findItemOpt.get();
 
-        Cart cart = getOrCreateCart(session);
-        cart.removeItem(findItem);
-        CartDto cartDto = new CartDto(cart);
-        return ResponseEntity.ok(cartDto); //삭제 후 전체 장바구니 상태 반환
+//        Cart cart = getOrCreateCart(session);
+
+        cartService.deleteItem(findItem.getId(), session);
+
+//        cart.removeItem(findItem);
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        return cartRepository.findByUser(loginUser)
+                .map(cart -> ResponseEntity.ok(new CartDto(cart)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+
+//        CartDto cartDto = new CartDto(cart);
+//        return ResponseEntity.ok(cartDto); //삭제 후 전체 장바구니 상태 반환
     }
 
 
